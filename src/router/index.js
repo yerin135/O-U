167719a556bc2b0b56e6af1531630aaa2a009a02
yerin jeon <rin9682@daum.net>
2021-06-13 15:09:49 +0900

    add pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Main from '../pages/Main';
 import Settings from '../pages/Settings';
+import Diary from '../pages/Diary';
+import Calender from '../pages/Calender';
 
 export default () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Main} />
       <Route exact path="/settings" component={Settings} />
+      <Route exact path="/diary" component={Diary} />
+      <Route exact path="/calender" component={Calender} />
       <Redirect to="/" />
     </Switch>
   </Router>
